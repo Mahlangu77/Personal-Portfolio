8ef5193a53c448c42a83a2c89320e4aa695987d4
Mahlangu77 <164662522+Mahlangu77@users.noreply.github.com>
2024-09-08 03:28:37 +0200

    Update JQuery.js
    Signed-off-by: Mahlangu77 <164662522+Mahlangu77@users.noreply.github.com>

--- JQuery.js
+++ JQuery.js
@@ -1,5 +1,3 @@
-/**import from index.html**/
-
 let menuIcon = document.querySelector('menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('sections');
